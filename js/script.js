f33ef14b387799f9d0d565788bfa0ce6a9cab951
Prince Azaad Manandhar <azaad.manandhar@gmail.com>
2018-10-25 15:15:31 +0100

    Made changes to all sheets; added multiple homepage changes; added modal popups.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 const hello = document.getElementById("hello");
 //console.log(hello);
-
 function greeting() {
   alert("BOO!");
   hello.textContent = "helolllwjdklewhdiwh";
 }
+hello.addEventListener("click", greeting);
+
+
+
+// Get the modal
+let modal = document.getElementById('WholeModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.getElementById('OGImg');
+const img2 = document.getElementById('OGImg2');
+const img3 = document.getElementById('OGImg3');
+let modalImg = document.getElementById("BigImageID");
+let captionText = document.getElementById("ImageName");
+
+
+function OpenModel(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img.addEventListener("click",OpenModel);
+img2.addEventListener("click",OpenModel);
+img3.addEventListener("click",OpenModel);
+
+
+const close = document.getElementsByClassName("CloseWindow")[0];
+function Closed(){
+    modal.style.display = "none";
+}
+close.addEventListener("click", Closed);
+
+// When the user clicks on <span> (x), close the modal
